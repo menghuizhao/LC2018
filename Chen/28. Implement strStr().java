f92102d408b004af74f361f0e3485bcc5350c4eb
Jingxuan Chen <jingxuan0601@gmail.com>
2018-01-15 22:03:51 -0800

    28 70

---
+++ Chen/28. Implement strStr().java
@@ -0,0 +1,19 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        if (haystack == null || needle == null || needle.length() > haystack.length()) {
+            return -1;
+        }
+        if (haystack.length() == 0) {
+            return 0;
+        }
+        
+        
+        for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+            if (haystack.substring(i, i + needle.length()).equals(needle)) {
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+}
